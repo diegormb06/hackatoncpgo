61e35d26f44d9b9df275a67f77e5717cc65e4175
Diego Rodrigues <diego.rmborges@seduc.go.gov.br>
2021-08-27 16:16:03 -0300

    test(ImageService): criada pasta para teste de imagem

--- test/ImageService.spec.ts
+++ test/ImageService.spec.ts
@@ -2,19 +2,26 @@ import test from "japa";
 import fs from "fs";
 import path from "path";
 import supertest from "supertest";
+import * as faker from "faker";
+import { UserFactory } from "Database/factories/userFactory";
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`;
 const api = supertest(BASE_URL + "/api");
 const basePath = path.join(__dirname, "..", "uploads", "photos/");
 
 test.group("Test ImageService", () => {
-  test("ImageService.uploadPhoto should upload and save a user Photo", async (assert) => {
-    const { text } = await api
-      .post("/uploads/photo/1")
-      .attach("photo", `${__dirname}/foto.jpg`)
-      .expect(200);
+  test.only("ImageService.uploadPhoto should upload and save a user Photo", async (assert) => {
+    // const userID = (await UserFactory.create()).id;
+    const avatar = fs.readFileSync(
+      "https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg"
+    );
+    console.log("buffer", avatar);
+    // const { text } = await api
+    //   .post(`/uploads/photo/${userID}`)
+    //   .attach("photo", avatar, "avatar.jpg")
+    //   .expect(200);
 
-    const newImage = JSON.parse(text).photo;
-    assert.isOk(fs.existsSync(`${basePath}/${newImage}`));
+    // const newImage = JSON.parse(text).photo;
+    // assert.isOk(fs.existsSync(`${basePath}/${newImage}`));
   });
 
   test("ImageService.uploadProductImages should upload one or many product images", async (assert) => {
